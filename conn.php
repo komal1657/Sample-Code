5ac9b4c9cbe174d3364d502176d6f2ac0afa8fcc
komal1657 <68587200+komal1657@users.noreply.github.com>
2020-07-21 15:14:04 +0530

    Create conn.php

---
+++ conn.php
@@ -0,0 +1,13 @@
+
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "login_db";
+
+
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $db_name);
+
+?> 
